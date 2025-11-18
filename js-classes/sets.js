d397267be2db624d8aeab868c4d7433d7523b318
shahan <shahanverse@gmail.com>
2025-11-18 16:58:41 +0530

    sets qes

---
+++ js-classes/sets.js
@@ -0,0 +1,35 @@
+1.// const emails = new Set()
+
+// emails.add('shahan@gmail.com')
+// emails.add('jabin@gmail.com')
+// emails.add('shahan@gmail.com')
+// emails.add('naja@gmail.com')
+
+// console.log(emails)
+
+2.
+// const numbers = [1,2,1,3,4,5,6,5]
+
+// const uniq = [...new Set(numbers)]
+
+// console.log(uniq)
+
+3.
+// const user = new Set(['shahan', 'alex'])
+
+// const newUser = 'jabin'
+
+// if (user.has(newUser)) {
+//   console.log('user already exits')
+// }else{
+//   user.add(newUser) 
+//   console.log('user added')
+// }
+
+4.
+  // const frontend = new Set(['html', 'css', 'javascript'])
+  // const backend = new Set(['mongodb', 'nodeJs', 'css'])
+
+  // const skills = new Set([...frontend, ...backend])
+
+  // console.log(skills)
