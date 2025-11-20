aca24a7f5afe03a7201b255b87e77fad2d33cfa7
shahan <shahanverse@gmail.com>
2025-11-20 20:58:30 +0530

    5 calback qes done

--- js-qes/callBackqes.js
+++ js-qes/callBackqes.js
@@ -15,7 +15,25 @@
 // console.log(result)
 
 3.
-const arr = [10, 35, 90, 50, 75];
+// const arr = [10, 35, 90, 50, 75];
 
-const secondLargest = arr.sort((a, b) => b - a)[1];
-console.log(secondLargest)
+// const secondLargest = arr.sort((a, b) => b - a)[1];
+// console.log(secondLargest)
+
+4.
+// const arr = [5, 10, 15, 20, 25, 30]
+
+// const sum = arr
+// .filter(n => n % 2 === 0 )
+// .reduce((acc, cur) => acc + cur , 0)
+// console.log(sum)
+
+5.
+// const arr = ['javascript', 'is', 'awesome']
+
+// const join = arr.join(' ');
+
+// console.log(join);
+
+6.
+
