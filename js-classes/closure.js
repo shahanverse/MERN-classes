da96564b160b6e91c0fb7bc7b314b16b76031394
shahan <shahanverse@gmail.com>
2025-11-28 13:30:31 +0530

    closure

--- js-classes/closure.js
+++ js-classes/closure.js
@@ -5,24 +5,53 @@
 //           used frequently in js frameworks : React , Vue , angular
 
 
-function outer(){
-  let x = 10 
+// function outer(){
+//   let x = 10 
 
-  function inner(){
-    console.log(x);
+//   function inner(){
+//     console.log(x);
     
-  }
-  return inner
-}
+//   }
+//   return inner
+// }
 
-const fn = outer()
+// const fn = outer()
 
-fn()
+// fn()
 
-//another expl
+// //another expl
 
-function out(){
-  let x = 10
+// function out(){
+//   let x = 10
 
-  function inner1
-}
+//   function inner1(){
+//     x++
+//     console.log(x);
+    
+//   }
+//   return inner1
+// }
+
+// const counter = out()
+// counter()
+// counter()
+
+
+// let array = [2,5,4,7]
+// let a = array [0]
+// let b = array [1]
+// console.log(a,b);
+
+// let [a1, ,c1] = array 
+
+// console.log(a1, c1);
+
+// let obj = {
+//   name: 'shahan',
+//   age: 22
+// }
+
+// let { name , age} = obj
+// console.log(name,age);
+
+
